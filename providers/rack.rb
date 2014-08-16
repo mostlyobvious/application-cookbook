7c016c2a1630e409012ac9e6c93bba9d35065c23
Paweł Pacana <pawel.pacana@gmail.com>
2014-08-16 20:26:28 +0200

    Workarounding SSL support for Rack provider.
    In the long run this needs redesign - there's no point in _ssl provider
    counterparts like html and html_ssl currently.

--- providers/rack.rb
+++ providers/rack.rb
@@ -11,18 +11,45 @@ action :create do
 end
 
 def configure_vhost
+  variables = {
+    :name       => application_name,
+    :root       => public_dir,
+    :upstream   => nginx_upstream(connection_string),
+    :domains    => application_domains.join(" "),
+    :access_log => access_log,
+    :error_log  => error_log
+  }
+
+  if ssl
+    variables[:ssl] = {
+      :certificate => ssl_certificate_path,
+      :key         => ssl_key_path
+    }
+
+    ssl_key_path = "/etc/nginx/#{vhost_name}.key"
+    file ssl_key_path do
+      content ssl_key
+      owner   "root"
+      group   "root"
+      mode    "644"
+    end
+
+    ssl_certificate_path = "/etc/nginx/#{vhost_name}.crt"
+    file ssl_certificate_path do
+      content ssl_certificate
+      owner   "root"
+      group   "root"
+      mode    "644"
+    end
+  end
+
   r = template "/etc/nginx/sites-available/#{vhost_name}" do
     cookbook "application"
     source "site"
     owner  "root"
     group  "root"
     mode   "644"
-    variables :name       => application_name,
-              :root       => public_dir,
-              :upstream   => nginx_upstream(connection_string),
-              :domains    => application_domains.join(" "),
-              :access_log => access_log,
-              :error_log  => error_log
+    variables variables
     notifies :reload, "service[nginx]"
   end
   new_resource.updated_by_last_action(true) if r.updated_by_last_action? ||
